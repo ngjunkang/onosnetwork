db7361ad0bbef632fba9a725a51f4148ab7106c8
Ng Jun Kang <ng_jun_kang@hotmail.com>
2023-11-05 11:46:38 +0800

    Create mininet

--- topos/ring.py
+++ topos/ring.py
@@ -12,20 +12,30 @@ class TreeTopo( Topo ):
 
     def build( self ):
         # Read ring.in
-	with open('ring.in') as f:
-		f.
         # Load configuration of Hosts, Switches, and Links
         # You can write other functions as you need.
+        lines = []
+        with open('ring.in') as f:
+            lines = [line.strip() for line in f]
+        no_hosts, no_switches = list(map(int, lines[0].split()))
+        links = [(line.split()[0], line.split()[0]) for line in lines[1:]]
 
         # Add hosts
         # > self.addHost('h%d' % [HOST NUMBER])
+        for i in range(1, no_hosts + 1):
+            self.addHost('h%d' % i)
 
         # Add switches
         # > sconfig = {'dpid': "%016x" % [SWITCH NUMBER]}
         # > self.addSwitch('s%d' % [SWITCH NUMBER], **sconfig)
+        for i in range(1, no_switches + 1):
+            sconfig = {'dpid': '%016x' % i}
+            self.addSwitch('s%d' % i, **sconfig)
 
         # Add links
         # > self.addLink([HOST1], [HOST2])
+        for node1, node2 in links:
+            self.addLink(node1, node2)
         
                     
 topos = { 'sdnip' : ( lambda: TreeTopo() ) }
